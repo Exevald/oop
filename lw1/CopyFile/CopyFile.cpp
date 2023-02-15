7fadccdbe8af412cce465e5fe950952dae3f14b8
Konstantin <64555204+Exevald@users.noreply.github.com>
2023-02-15 23:56:08 +0300

    Реализовал копирование текстового файла

---
+++ lw1/CopyFile/CopyFile.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <fstream>
+#include <optional>
+#include <string>
+
+struct Args
+{
+	std::string inputFileName;
+	std::string outputFileName;
+};
+
+std::optional<Args> ParseArguments(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		std::cout << "Invalid argumants count" << std::endl;
+		std::cout << "Usage: CopyFile.exe <input file name> <output file name>" << std::endl;
+		return std::nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	args.outputFileName = argv[2];
+	return args;
+}
+
+void CopyStreams(std::ifstream& input, std::ofstream& output)
+{
+	// Копируем содержимое входного файла в выходной
+	char ch;
+	while (input.get(ch))
+	{
+		if (!output.put(ch))
+		{
+			break;
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArguments(argc, argv);
+	// Проверка правильности аргуметнов командной строки
+	if (!args)
+	{
+		return 1;
+	}
+
+	// Открываем входной файл
+	std::ifstream input;
+	input.open(args->inputFileName);
+	if (!input.is_open())
+	{
+		std::cout << "Failed to open '" << args->inputFileName << "' for reading" << std::endl;
+		return 1;
+	}
+
+	// Открываем выходной файл
+	std::ofstream output;
+	output.open(args->outputFileName);
+	if (!output.is_open())
+	{
+		std::cout << "Failed to open '" << args->outputFileName << "' for writing" << std::endl;
+		return 1;
+	}
+
+	CopyStreams(input, output);
+
+	if (input.bad())
+	{
+		std::cout << "Failed to read data from input file" << std::endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ lw1/CopyFile/tests.bat
@@ -0,0 +1,46 @@
+﻿@echo off
+
+REM Путь к тестируемой программе передаётся через 1-ый аргумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+)
+
+REM Копируем пустой файл
+%MyProgram% empty.txt "%TEMP%\output.txt" || goto err
+fc empty.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 1 passed
+
+REM Копируем файл из одной строки
+%MyProgram% one-line.txt "%TEMP%\output.txt" || goto err
+fc one-line.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 2 passed
+
+REM Копируем файл из нескольких строк
+%MyProgram% multiline.txt "%TEMP%\output.txt" || goto err
+fc multiline.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 3 passed
+
+REM Копируем отсутствующий файл, программа должна прекратить работу
+%MyProgram% MissingFile.txt "%TEMP%\output.txt" && goto err
+echo Test 4 passed
+
+REM Не задан файл для вывода, программа должна прекратить работу
+%MyProgram% MissingFile.txt && goto err
+echo Test 5 passed
+
+REM Не заданы файлы для ввода и вывода, программа должна прекратить работу
+%MyProgram% && goto err
+echo Test 6 passed
+
+REM Тесты прошли успешно
+echo All tests passed successfully
+exit /B 0
+
+REM Ошибки
+:err
+echo Test failed
+exit /B 1
