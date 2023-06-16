39b41dfb7b515702a9907eee77361b8829ca46cc
Константин Калинин <exevald@outlook.com>
2023-06-16 21:32:09 +0300

    Реализовал программу Replace и добаил для неё тесты

---
+++ lw1/Replace/Replace.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <optional>
+
+struct Args
+{
+	std::string inputFileName;
+	std::string outputFileName;
+	std::string searchString;
+	std::string replaceString;
+};
+
+std::optional<Args> ParseArguments(int argc, char* argv[])
+{
+	if (argc != 5)
+	{
+		std::cout << "Invalid argument count" << std::endl
+			<< "Usage: replace.exe <inputFile> <outputFile> <searchString> <replacementString>" << std::endl;
+		return std::nullopt;
+	}
+	Args args;
+	args.inputFileName = argv[1];
+	args.outputFileName = argv[2];
+	args.searchString = argv[3];
+	args.replaceString = argv[4];
+	return args;
+}
+
+std::string ReplaceString(const std::string& subject, const std::string& searchString, const std::string& replacementString)
+{
+	if (searchString.empty()) {
+		return subject;
+	}
+	size_t pos = 0;
+	std::string result = subject;
+
+	while ((pos = result.find(searchString, pos)) != std::string::npos) {
+		result.replace(pos, searchString.length(), replacementString);
+		pos += replacementString.length();
+	}
+	return result;
+}
+
+void CopyStreamWithReplacement(std::istream& input, std::ostream& output, const std::string& searchString, const std::string& replacementString)
+{
+	std::string line;
+
+	while (std::getline(input, line))
+	{
+		output << ReplaceString(line, searchString, replacementString) << std::endl;
+	}
+}
+
+int CopyFileWithReplacement(std::string& inputFileName, std::string& outputFileName, const std::string& searchString, const std::string& replacementString)
+{
+	std::ifstream inputFile;
+	inputFile.open(inputFileName);
+
+	std::ofstream outputFile;
+	outputFile.open(outputFileName);
+
+	if (!inputFile.is_open())
+	{
+		std::cout << "Failed to open " << inputFileName << " for reading" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (!outputFile.is_open())
+	{
+		std::cout << "Failed to open " << outputFileName << " for writing" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	CopyStreamWithReplacement(inputFile, outputFile, searchString, replacementString);
+
+}
+
+int main(int argc, char* argv[])
+{
+	auto args = ParseArguments(argc, argv);
+	if (!args)
+	{
+		return EXIT_FAILURE;
+	}
+
+	CopyFileWithReplacement(args->inputFileName, args->outputFileName, args->searchString, args->replaceString);
+	return EXIT_SUCCESS;
+}
---
+++ lw1/Replace/tests.bat
@@ -0,0 +1,38 @@
+@echo off
+
+REM Путь к тестируемой программе передаётся через 1-ый аргумент командной строки
+SET MyProgram="%~1"
+
+REM Защита от запуска без аргумента, задающего путь к программе
+if %MyProgram%=="" (
+	echo Please specify path to program
+	exit /B 1
+)
+
+REM Не заданы файлы для ввода и вывода, программа должна прекратить работу
+%MyProgram% && goto err
+echo Test 1 passed
+
+REM При запуске с правильными параметрами ожидается ненулевой код возврата
+%MyProgram% test-data\favourite-language.txt "%TEMP%\favourite-language.txt" PHP GO || goto err
+fc test-data\favourite-language-replace-php-with-golang.txt "%TEMP%\favourite-language.txt" > nul || goto err
+echo Test 2 passed
+
+REM При ненахождении подстроки в строке программа должна вывести искомый текст
+%MyProgram% test-data\favourite-language.txt "%TEMP%\favourite-language.txt" Python GO || goto err
+fc test-data\favourite-language.txt "%TEMP%\favourite-language.txt" > nul || goto err
+echo Test 3 passed
+
+REM Проверка на отсутствие зацикливания программы при замене 1231234 в строке 12312312345 
+%MyProgram% test-data\number-string.txt "%TEMP%\number-string.txt" 1231234 111 || goto err
+fc test-data\number-string-replace-1231234-with-111.txt "%TEMP%\input2.txt" > nul || goto err
+echo Test 4 passed
+
+REM Тесты прошли успешно
+echo All tests passed successfully
+exit /B 0
+
+REM Ошибки
+:err
+echo Test failed
+exit /B 1
