ffe11ea459cc34ac063f8a58bcbb81d4442356b6
Konstantin Kalinin <kalinin.konst.official@gmail.com>
2025-03-12 15:08:20 +0300

    Сделал задание Date и написал для него тесты

---
+++ lw5/Date/src/CDate.cpp
@@ -0,0 +1,279 @@
+#include "CDate.h"
+#include <limits>
+
+CDate::CDate(unsigned day, Month month, unsigned year)
+{
+	unsigned timestamp = ConvertDateToTimestamp(day, month, year);
+	if (timestamp == static_cast<unsigned>(-1))
+	{
+		m_timestamp = std::numeric_limits<unsigned>::max();
+	}
+	else
+	{
+		m_timestamp = timestamp;
+	}
+}
+
+CDate::CDate(unsigned timestamp)
+	: m_timestamp(timestamp)
+{
+	if (!IsValid())
+	{
+		m_timestamp = 0;
+	}
+}
+
+CDate::CDate()
+{
+	m_timestamp = 0;
+}
+
+CDate& CDate::operator++()
+{
+	if (IsValid())
+	{
+		++m_timestamp;
+	}
+	return *this;
+}
+
+CDate CDate::operator++(int)
+{
+	CDate temp = *this;
+	++(*this);
+	return temp;
+}
+
+CDate& CDate::operator--()
+{
+	if (IsValid() && m_timestamp > 0)
+	{
+		--m_timestamp;
+	}
+	return *this;
+}
+
+CDate CDate::operator--(int)
+{
+	CDate temp = *this;
+	--(*this);
+	return temp;
+}
+
+CDate CDate::operator+(int days) const
+{
+	return CDate(m_timestamp + days);
+}
+
+CDate CDate::operator-(int days) const
+{
+	return CDate(m_timestamp >= days ? m_timestamp - days : 0);
+}
+
+int CDate::operator-(const CDate& other) const
+{
+	return static_cast<int>(m_timestamp) - static_cast<int>(other.m_timestamp);
+}
+
+CDate& CDate::operator+=(int days)
+{
+	if (IsValid())
+	{
+		m_timestamp += days;
+	}
+	return *this;
+}
+
+CDate& CDate::operator-=(int days)
+{
+	if (IsValid() && m_timestamp >= days)
+	{
+		m_timestamp -= days;
+	}
+	else
+	{
+		m_timestamp = 0;
+	}
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& stream, const CDate& date)
+{
+	if (date.IsValid())
+	{
+		unsigned day = date.GetDay();
+		Month month = date.GetMonth();
+		unsigned year = date.GetYear();
+		stream << (day < 10 ? "0" : "") << day << "."
+			   << (static_cast<unsigned>(month) < 10 ? "0" : "") << static_cast<unsigned>(month) << "."
+			   << year;
+	}
+	else
+	{
+		stream << "INVALID DATE";
+	}
+	return stream;
+}
+
+std::istream& operator>>(std::istream& stream, CDate& date)
+{
+	unsigned day, year;
+	unsigned month;
+	char sep1, sep2;
+	stream >> day >> sep1 >> month >> sep2 >> year;
+	if (sep1 == '.' && sep2 == '.')
+	{
+		date.m_timestamp = CDate::ConvertDateToTimestamp(day, static_cast<Month>(month), year);
+	}
+	else
+	{
+		date.m_timestamp = 0;
+	}
+	return stream;
+}
+
+bool CDate::operator==(const CDate& date) const
+{
+	return m_timestamp == date.m_timestamp;
+}
+
+bool CDate::operator!=(const CDate& date) const
+{
+	return m_timestamp != date.m_timestamp;
+}
+
+bool CDate::operator>(const CDate& date) const
+{
+	return m_timestamp > date.m_timestamp;
+}
+
+bool CDate::operator<(const CDate& date) const
+{
+	return m_timestamp < date.m_timestamp;
+}
+
+bool CDate::operator>=(const CDate& date) const
+{
+	return m_timestamp >= date.m_timestamp;
+}
+
+bool CDate::operator<=(const CDate& date) const
+{
+	return m_timestamp <= date.m_timestamp;
+}
+
+unsigned CDate::GetDay() const
+{
+	unsigned day, year;
+	Month month;
+	ConvertTimestampToDate(day, month, year);
+	return day;
+}
+
+Month CDate::GetMonth() const
+{
+	unsigned day, year;
+	Month month;
+	ConvertTimestampToDate(day, month, year);
+	return month;
+}
+
+unsigned CDate::GetYear() const
+{
+	unsigned day, year;
+	Month month;
+	ConvertTimestampToDate(day, month, year);
+	return year;
+}
+
+WeekDay CDate::GetWeekDay() const
+{
+	if (!IsValid())
+	{
+		return WeekDay::SUNDAY;
+	}
+	return static_cast<WeekDay>((m_timestamp + 4) % 7);
+}
+
+bool CDate::IsValid() const
+{
+	unsigned day, year;
+	Month month;
+	ConvertTimestampToDate(day, month, year);
+
+	return year >= 1970 && year <= 9999 && month >= Month::JANUARY && month <= Month::DECEMBER && day >= 1 && day <= GetDaysInMonth(month, year);
+}
+
+bool CDate::IsLeapYear(unsigned year)
+{
+	return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+unsigned CDate::GetDaysInMonth(Month month, unsigned year)
+{
+	switch (month)
+	{
+	case Month::FEBRUARY:
+		return IsLeapYear(year) ? 29 : 28;
+	case Month::APRIL:
+	case Month::JUNE:
+	case Month::SEPTEMBER:
+	case Month::NOVEMBER:
+		return 30;
+	default:
+		return 31;
+	}
+}
+
+void CDate::ConvertTimestampToDate(unsigned& day, Month& month, unsigned& year) const
+{
+	unsigned remainingDays = m_timestamp;
+	year = 1970;
+
+	while (true)
+	{
+		unsigned daysInYear = IsLeapYear(year) ? 366 : 365;
+		if (remainingDays < daysInYear)
+		{
+			break;
+		}
+		remainingDays -= daysInYear;
+		++year;
+	}
+
+	month = Month::JANUARY;
+	while (true)
+	{
+		unsigned daysInCurrentMonth = GetDaysInMonth(month, year);
+		if (remainingDays < daysInCurrentMonth)
+		{
+			break;
+		}
+		remainingDays -= daysInCurrentMonth;
+		month = static_cast<Month>(static_cast<unsigned>(month) + 1);
+	}
+
+	day = remainingDays + 1;
+}
+
+unsigned CDate::ConvertDateToTimestamp(unsigned day, Month month, unsigned year)
+{
+	if (year < 1970 || year > 9999 || month < Month::JANUARY || month > Month::DECEMBER || day < 1 || day > GetDaysInMonth(month, year))
+	{
+		return -1;
+	}
+
+	unsigned totalDays = 0;
+	for (unsigned y = 1970; y < year; ++y)
+	{
+		totalDays += IsLeapYear(y) ? 366 : 365;
+	}
+
+	for (unsigned m = 1; m < static_cast<unsigned>(month); ++m)
+	{
+		totalDays += GetDaysInMonth(static_cast<Month>(m), year);
+	}
+
+	totalDays += day - 1;
+	return totalDays;
+}
---
+++ lw5/Date/test/Date_test.cpp
@@ -0,0 +1,115 @@
+#include "CDate.h"
+#include <gtest/gtest.h>
+
+TEST(Date, TestDefaultConstructor)
+{
+	CDate date;
+	ASSERT_EQ(date.GetDay(), 1);
+	ASSERT_EQ(date.GetMonth(), Month::JANUARY);
+	ASSERT_EQ(date.GetYear(), 1970);
+	ASSERT_EQ(date.GetWeekDay(), WeekDay::THURSDAY);
+	ASSERT_TRUE(date.IsValid());
+}
+
+TEST(Date, TestConstructorWithParameters)
+{
+	CDate date(12, Month::MARCH, 2025);
+	ASSERT_EQ(date.GetDay(), 12);
+	ASSERT_EQ(date.GetMonth(), Month::MARCH);
+	ASSERT_EQ(date.GetYear(), 2025);
+	ASSERT_TRUE(date.IsValid());
+
+	CDate date2(28, Month::OCTOBER, 9999);
+	ASSERT_TRUE(date2.IsValid());
+}
+
+TEST(Date, TestInvalidDate)
+{
+	CDate date1(31, Month::FEBRUARY, 2023);
+	ASSERT_FALSE(date1.IsValid());
+
+	CDate date2(10, Month::OCTOBER, 10000);
+	ASSERT_FALSE(date2.IsValid());
+
+	CDate date4(28, Month::OCTOBER, 1969);
+	ASSERT_FALSE(date4.IsValid());
+}
+
+TEST(Date, TestIncrementOperator)
+{
+	CDate date(28, Month::FEBRUARY, 2020);
+	++date;
+	ASSERT_EQ(date.GetDay(), 29);
+	ASSERT_EQ(date.GetMonth(), Month::FEBRUARY);
+	ASSERT_EQ(date.GetYear(), 2020);
+}
+
+TEST(Date, TestDecrementOperator)
+{
+	CDate date(1, Month::MARCH, 2023);
+	--date;
+	ASSERT_EQ(date.GetDay(), 28);
+	ASSERT_EQ(date.GetMonth(), Month::FEBRUARY);
+	ASSERT_EQ(date.GetYear(), 2023);
+}
+
+TEST(Date, TestAdditionOperator)
+{
+	CDate date(28, Month::FEBRUARY, 2020);
+	CDate newDate = date + 2;
+	ASSERT_EQ(newDate.GetDay(), 1);
+	ASSERT_EQ(newDate.GetMonth(), Month::MARCH);
+	ASSERT_EQ(newDate.GetYear(), 2020);
+}
+
+TEST(Date, TestSubtractionOperator)
+{
+	CDate date(1, Month::MARCH, 2023);
+	CDate newDate = date - 2;
+	ASSERT_EQ(newDate.GetDay(), 27);
+	ASSERT_EQ(newDate.GetMonth(), Month::FEBRUARY);
+	ASSERT_EQ(newDate.GetYear(), 2023);
+}
+
+TEST(Date, TestDifferenceBetweenDates)
+{
+	CDate date1(1, Month::MARCH, 2023);
+	CDate date2(28, Month::FEBRUARY, 2023);
+	ASSERT_EQ(date1 - date2, 1);
+}
+
+TEST(Date, TestInputOutputOperator)
+{
+	CDate date;
+	std::istringstream input("15.03.2023");
+	input >> date;
+	ASSERT_EQ(date.GetDay(), 15);
+	ASSERT_EQ(date.GetMonth(), Month::MARCH);
+	ASSERT_EQ(date.GetYear(), 2023);
+
+	std::ostringstream output;
+	output << date;
+	ASSERT_EQ(output.str(), "15.03.2023");
+}
+
+TEST(Date, TestDatesComparing)
+{
+	CDate date1(12, Month::MARCH, 2025);
+	CDate date2(12, Month::APRIL, 2025);
+	ASSERT_TRUE(date2 > date1);
+	ASSERT_TRUE(date2 >= date1);
+
+	CDate date3(11, Month::MARCH, 2025);
+	ASSERT_TRUE(date3 < date1);
+	ASSERT_TRUE(date3 <= date1);
+
+	CDate date4(12, Month::MARCH, 2025);
+	ASSERT_TRUE(date1 == date4);
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
